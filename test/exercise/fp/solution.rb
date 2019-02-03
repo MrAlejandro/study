a019df2c0f05cc5bbc8d6f093fbe2ac291114a23
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-02-03 22:49:07 +0400

    Average rating task solved.

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,15 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        summary =
+          array
+          .select { |film| film['country'].to_s.split(',').size > 1 && film['rating_kinopoisk'].to_f.positive? }
+          .reduce(rating: 0.0, qty: 0) do |acc, movie|
+            {rating: acc[:rating] + movie['rating_kinopoisk'].to_f, qty: acc[:qty] + 1}
+          end
+
+        summary[:rating] / summary[:qty]
       end
 
       def chars_count(_films, _threshold)
