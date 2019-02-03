81ff9b10b2858a0b22ca5f50f23102ec20296b03
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-02-03 21:56:42 +0400

    Binary search task solved.

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,7 +6,21 @@ module Exercise
         array.map { |num| num.positive? ? max : num }
       end
 
-      def search(_array, _query)
+      def search(array, query)
+        size = array.size
+        return -1 if size.zero?
+
+        iter = lambda do |left, right|
+          return -1 if right < left
+
+          mid_index = left + (right - left) / 2
+          return mid_index if array[mid_index] == query
+
+          left, right = array[mid_index] > query ? [left, mid_index - 1] : [mid_index + 1, right]
+          iter.call(left, right)
+        end
+
+        iter.call(0, size - 1)
       end
     end
   end
