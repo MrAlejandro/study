0a86da70562e80fa205829fa578fa7890bcbb9b2
Aleksandr Scherbachenko <ascherbachenko@cs-cart.com>
2019-02-04 13:58:00 +0400

    Some code style changes applied.

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -9,7 +9,7 @@ module Exercise
           films
           .select { |film| film['country'].to_s.split(',').size > 1 && film['rating_kinopoisk'].to_f.positive? }
           .reduce(rating: 0.0, qty: 0) do |acc, movie|
-            {rating: acc[:rating] + movie['rating_kinopoisk'].to_f, qty: acc[:qty] + 1}
+            { rating: acc[:rating] + movie['rating_kinopoisk'].to_f, qty: acc[:qty] + 1 }
           end
 
         summary[:rating] / summary[:qty]
@@ -17,8 +17,8 @@ module Exercise
 
       def chars_count(films, threshold)
         films
-        .select { |film| film['rating_kinopoisk'].to_f >= threshold }
-        .reduce(0) { |qty, film| qty += film["name"].count('и') }
+          .select { |film| film['rating_kinopoisk'].to_f >= threshold }
+          .reduce(0) { |qty, film| qty + film['name'].count('и') }
       end
     end
   end
